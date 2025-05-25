f000b2cc366f2aa16c26231aa0444c874fc21eb6
espindolae <97189104+espindolae@users.noreply.github.com>
2025-05-24 21:53:16 -0700

    list-app
    Created items.html

---
+++ list-app/items.js
@@ -0,0 +1,6 @@
+/** Title: List Manager Project
+ *  Class: CSC3221 Group VI
+ *  Purpose: Client-side controller logic for the List Manager UI.
+ *  Authors: Bria Tran, Eva Espindola, Ryan Fecarotta
+ *  Date: May 24th, 2025
+ */
